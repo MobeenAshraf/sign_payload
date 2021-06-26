52af856eb8a120df9dcb4b8e9e254ddeffeb28aa
Mobeen <mobeen.dude786@gmail.com>
2021-06-26 21:02:04 +0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module rosetta_sign
+
+go 1.16
+
+require github.com/coinbase/rosetta-sdk-go v0.6.10
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/coinbase/rosetta-sdk-go/keys"
+	"github.com/coinbase/rosetta-sdk-go/types"
+)
+
+func main() {
+	privKeyHex := ""
+	senderAddress := ""
+	signingMessage := ""
+	const (
+		payloadSigType types.SignatureType = "ecdsa_recovery"
+		sigType        types.SignatureType = "ecdsa_recovery"
+	)
+
+	signingPayloadHexDecoded, _ := hex.DecodeString(signingMessage)
+	keyPair, err := keys.ImportPrivateKey(privKeyHex, types.Secp256k1)
+	if err != nil {
+		returnErr(err)
+		return
+	}
+
+	signer, _ := keyPair.Signer()
+	sig, err := signer.Sign(&types.SigningPayload{
+		AccountIdentifier: &types.AccountIdentifier{
+			Address: senderAddress,
+		},
+		Bytes:         signingPayloadHexDecoded,
+		SignatureType: sigType,
+	}, payloadSigType)
+	if err != nil {
+		returnErr(err)
+		return
+	}
+	fmt.Println(hex.EncodeToString(sig.Bytes))
+
+}
+
+func returnErr(err error) {
+	fmt.Println("Error:")
+	fmt.Println(err)
+}
